761c5c1adb6dafadefc76e366f4dfc77d584079a
P1roks <piotrekjakobczyk1@gmail.com>
2024-09-14 18:08:30 +0200

    add build to gitignore

--- build/routes/cartRoutes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cartRouter = void 0;
-const express_1 = require("express");
-exports.cartRouter = (0, express_1.Router)();
